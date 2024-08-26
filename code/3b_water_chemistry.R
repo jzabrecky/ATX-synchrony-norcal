8a9e6b635fee1f893430342c8d9b9cbd84fee121
jzabrecky <jordanmz@alumni.unc.edu>
2024-08-26 13:25:16 -0700

    added shimadzu data

--- code/3b_water_chemistry.R
+++ code/3b_water_chemistry.R
@@ -11,9 +11,32 @@
 # loading libraries
 lapply(c("tidyverse", "lubridate", "plyr"), require, character.only = T)
 
-# loading raw data
+## loading raw data
+
+# field parameters
 field_params <- ldply(list.files(path = "./data/field_and_lab/raw_data/", 
                                 pattern = "field_params"), function(filename) {
   d <- read.csv(paste("data/field_and_lab/raw_data/", filename, sep = ""))
   return(d)
 })
+
+# aq400 values
+aq400 <- ldply(list.files(path = "./data/field_and_lab/raw_data/",
+                          pattern = "aq400"), function(filename) {
+  d <- read.csv(paste("data/field_and_lab/raw_data/", filename, sep = ""))
+  return(d)
+})
+
+# shimadzu values
+shimadzu <- ldply(list.files(path = "./data/field_and_lab/raw_data/",
+                             pattern = "shimadzu"), function(filename) {
+  d <- read.csv(paste("data/field_and_lab/raw_data/", filename, sep = ""))
+  return(d)
+})
+
+# ion-chromatography cation/anion data (only one year of this data)
+IC <- read.csv("./data/field_and_lab/raw_data/IC_2022.csv")
+# TBD what to do with this data
+
+#### (2) Processing AQ400 values ####
+
