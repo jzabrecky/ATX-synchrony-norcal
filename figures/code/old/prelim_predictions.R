6f85a3a2a114c70abbf48ffd2e7e46dfaba27c7f
jzabrecky <jordanmz@alumni.unc.edu>
2025-06-20 12:24:35 -0700

    ran test predictive models just using normal distribution

--- figures/code/old/prelim_predictions.R
+++ figures/code/old/prelim_predictions.R
@@ -9,6 +9,8 @@ atx_M <- read.csv("./data/predictive_models/predictions_M_atx.csv")
 atx_AC <- read.csv("./data/predictive_models/predictions_AC_cover.csv")
 observed <- read.csv("./data/predictive_models/inputs.csv") %>% 
   mutate(field_date = ymd(field_date))
+preds_M_normal_dist <- read.csv("./data/predictive_models/normal_test/predictions_M_cover.csv") %>% 
+  mutate(field_date = ymd(field_date))
 
 # plot colors
 palette <- c("#523939", "#528b87", "#416f16", "#62a7f8", "#8b9609", "gray", "#bdb000", "#90ac7c")
@@ -25,6 +27,23 @@ for(i in 1:length(preds_M_list)) {
                shape = 18) +
     labs(title = paste(preds_M_list[[i]]$model[1], "-- Microcoleus cover"), y = "% of max at reach") +
     facet_wrap(~site_reach) +
+    theme_bw()
+  print(plot)
+}
+
+# normal attempt
+preds_M_normal_dist_list <- split(preds_M_normal_dist, 
+                                  preds_M_normal_dist$model) # split into list
+
+for(i in 1:length(preds_M_normal_dist_list)) {
+  plot <- ggplot(data = preds_M_normal_dist_list[[i]], aes(x = field_date)) +
+    geom_ribbon(aes(ymin = ci_lower, ymax = ci_upper, group = 1), fill = ribbon_palette[i], alpha = 0.8) +
+    geom_point(aes(y = mean), size = 3, color = palette[i]) +
+    geom_point(data = observed, aes(y = resp_M_cover_norm), color = "black",
+               shape = 18) +
+    labs(title = paste(preds_M_normal_dist_list[[i]]$model[1], "-- Microcoleus cover"), y = "% of max at reach") +
+    facet_wrap(~site_reach) +
+    coord_cartesian(ylim = c(0,100)) +
     theme_bw()
   print(plot)
 }
