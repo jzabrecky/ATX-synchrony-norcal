9e7a8ac86f5340fe419f5563c845d659c3dd3eed
Zabrecky <jordanmz@alumni.unc.edu>
2025-11-26 13:22:31 -0800

    coauthor edits: inclusion of calculations for dissolved oxygen residence distance, MCY & CYL concentrations, GPP contextualization

--- code/misc_code/microcystins_cylindrospermopsins.R
+++ code/misc_code/microcystins_cylindrospermopsins.R
@@ -19,16 +19,18 @@ EDI <- read.csv("./data/EDI_data_package/anatoxin_concentrations.csv") %>%
 # subset for samples with microcystins detected
 microcystins <- EDI %>% 
   filter(MCY_ug_g != "ND") %>% 
-  select(sample_type, site_reach, field_date, MCY_ug_g)
+  select(sample_type, site_reach, field_date, MCY_ug_g, triplicate, MCY_det_limit)
 
 # save as csv to make table more easily copy & paste-able to supplemental methods
+write.csv(microcystins, "./data/field_and_lab/microcystins.csv",
+          row.names = FALSE)
 
 # upper and lower detection limits
-max(EDI$MCY_det_limit, na.rm = TRUE)
-min(EDI$MCY_det_limit, na.rm = TRUE)
+max(EDI$MCY_det_limit, na.rm = TRUE) # 0.21
+min(EDI$MCY_det_limit, na.rm = TRUE) # 0.06
 
 #### (3) Cylindrospermopsins ####
 
 # already know these are all non-detects but what are the upper & lower det. limitss?
-max(EDI$CYL_det_limit, na.rm = TRUE)
-min(EDI$CYL_det_limit, na.rm = TRUE)
+max(EDI$CYL_det_limit, na.rm = TRUE) # 0.1837
+min(EDI$CYL_det_limit, na.rm = TRUE) # 0.0029
--- figures/code/sfig_gpp_sfkeel_2023.R
+++ figures/code/sfig_gpp_sfkeel_2023.R
@@ -1,11 +1,9 @@
 #### Primary figure for taxa-specific cover & anatoxins and GPP on each river
 ### Jordan Zabrecky
-## last edited: 08.06.2025
+## last edited: 11.06.2025
 
-# This script creates a supplemental figure showing the difference
-# of magnitudes of GPP within the river
-
-# figure out why GPP ribbon is weird..?>?>
+# This script creates a supplemental figure showing the timing of peak
+# taxon-specific cover and anatoxin concentrations compared to GPP
 
 #### (1) Loading libraries and data ####
 
@@ -14,7 +12,8 @@ lapply(c("tidyverse", "lubridate", "plyr", "cowplot"),
        require, character.only = T)
 
 ## gpp data
-gpp <- rbind(read.csv("./data/metab_model_outputs_processed/sfkeel_mir_2023_metab.csv"),
+gpp <- rbind(read.csv("./data/metab_model_outputs_processed/sfkeel_mir_2022_metab.csv"),
+             read.csv("./data/metab_model_outputs_processed/sfkeel_mir_2023_metab.csv"),
              read.csv("./data/metab_model_outputs_processed/sfkeel_sth_2023_metab.csv")) %>% 
   mutate(date = ymd(date))
 
@@ -22,14 +21,18 @@ gpp <- rbind(read.csv("./data/metab_model_outputs_processed/sfkeel_mir_2023_meta
 disc <- rbind(read.csv("./data/USGS/sfkeel_mir_discharge_daily.csv"),
               read.csv("./data/USGS/sfkeel_sth_discharge_daily.csv")) %>% 
   mutate(date = ymd(date),
-         site_year = case_when(site == "sfkeel_mir" ~ "sfkeel_mir_2023",
-                               TRUE ~ "sfkeel_sth_2023"))
+         year = year(date),
+         site_year = case_when(site == "sfkeel_sth" ~ "sfkeel_sth_2023",
+                               year == 2022 & site == "sfkeel_mir" ~ "sfkeel_mir_2022",
+                               TRUE ~ "sfkeel_mir_2023"))
 
-# to calculate maximum for site for lines
+# to calculate maximum for site for lines for 2023 data
 cover <- read.csv("./data/field_and_lab/percover_bysite.csv") %>% 
   mutate(field_date = ymd(field_date),
          year = year(field_date)) %>% 
-  filter(year == 2023)
+  filter(site == "SFE-SH" | site == "SFE-M_all_sites") %>% 
+  mutate(site = case_when(site == "SFE-M_all_sites" ~ "SFE-M",
+                           TRUE ~ site))
 cover_list <- split(cover, cover$site)
 atx <- read.csv("./data/field_and_lab/sfkeel23_combined.csv") %>% 
   select(field_date, site_reach, site, TM_ATX_all_ug_orgmat_g, TAC_ATX_all_ug_orgmat_g) %>% 
@@ -43,8 +46,18 @@ atx <- read.csv("./data/field_and_lab/sfkeel23_combined.csv") %>%
                    mean_TAC_atx = mean(TAC_ATX_all_ug_orgmat_g))
 atx_list <- split(atx, atx$site)
 
-#### (2) Making Figures ####
+# do the same for 2022 data
+cover22 <- read.csv("./data/field_and_lab/allrivers22_combined.csv") %>% 
+  filter(site == "SFE-M") %>% 
+  mutate(field_date = ymd(field_date),
+         year = year(field_date)) %>% 
+  dplyr::group_by(site, field_date) %>% 
+  dplyr::summarize(mean_TM_atx = mean(TM_ATX_all_ug_orgmat_g, na.rm = TRUE),
+                   mean_TAC_atx = mean(TAC_ATX_all_ug_orgmat_g, na.rm = TRUE),
+                   mean_micro_cover = mean(microcoleus),
+                   mean_anacyl_cover = mean(anabaena_cylindrospermum))
 
+#### (2) Making Figures ####
 
 # set theme for all plots
 theme_set(theme_bw() + theme(legend.position = "bottom", 
@@ -53,23 +66,50 @@ theme_set(theme_bw() + theme(legend.position = "bottom",
                              text = element_text(size = 10), axis.ticks.length=unit(.25, "cm"),
                              axis.text = element_text(size = 10), title = element_text(size = 10)))
 
-# add segment column to avoid ribbon being drawn across plot when we weren't taking data
+## (a) south fork eel lower 2022
+
+# dates for max. taxon-specific cover and anatoxin (2022)
+mir_ac_22 <- cover22$field_date[which(cover22$mean_anacyl_cover == max(cover22$mean_anacyl_cover))]
+mir_m_22 <- cover22$field_date[which(cover22$mean_micro_cover == max(cover22$mean_micro_cover))]
+mir_ac_atx_22 <- cover22$field_date[which(cover22$mean_TAC_atx == max(cover22$mean_TAC_atx, na.rm = TRUE))]
+mir_m_atx_22 <- cover22$field_date[which(cover22$mean_TM_atx == max(cover22$mean_TM_atx))]
+
+# # add segment column to avoid ribbon being drawn across plot when we weren't taking data
 gpp$segment <- 1
-gpp$segment[104:105] <- 2
-gpp$segment[106:108] <- 3
-gpp$segment[109:nrow(gpp)] <- 4
+gpp$segment[14:nrow(gpp)] <- 2
+
+# plot
+mir_gpp22 <- ggplot(data = gpp %>% filter(site_year == "sfkeel_mir_2022"), aes(x = date)) +
+  geom_area(data = disc %>% filter(site_year == "sfkeel_mir_2022"), 
+            aes(y = discharge_m3_s * 2), fill = "#d9ecff") +
+  geom_ribbon(aes(ymin = GPP.2.5.pct, ymax = GPP.97.5.pct, group = segment),
+              fill = "#9ced66", alpha = 0.8) +
+  geom_point(aes(y = GPP.mean), color = "#397014", size = 1.2, alpha = 1) +
+  geom_vline(xintercept = mir_ac_22, color = "#8f8504", linewidth = 2, alpha = 1) +
+  geom_vline(xintercept = mir_m_22, color = "#2871c7", linewidth = 2, alpha = 1) +
+  geom_vline(xintercept = mir_ac_atx_22, color = "#d1c960", linewidth = 2, alpha = 1,
+             linetype = "longdash") +
+  geom_vline(xintercept = mir_m_atx_22, color = "#5a88bf", linewidth = 2, alpha = 1,
+             linetype = "dashed") +
+  scale_x_date(limits = as.Date(c("2022-06-20", "2022-09-27"))) +
+  scale_y_continuous(sec.axis = sec_axis(~ . / 2)) +
+  coord_cartesian(ylim = c(0, 10)) +
+  labs(y = NULL, x = NULL, title = "South Fork Eel Upper 2022")
+mir_gpp22
+
+## south fork eel lower 2023
 
 # dates for taxa-specific cover and anatoxin
-mir_ac <- cover_list$`SFE-M_all_sites`$field_date[which(cover_list$`SFE-M_all_sites`$anabaena_cylindrospermum_mean 
-                                               == max(cover_list$`SFE-M_all_sites`$anabaena_cylindrospermum_mean))]
-mir_m <- cover_list$`SFE-M_all_sites`$field_date[which(cover_list$`SFE-M_all_sites`$microcoleus_mean 
-                                              == max(cover_list$`SFE-M_all_sites`$microcoleus_mean))]
+mir_ac <- cover_list$`SFE-M`$field_date[which(cover_list$`SFE-M`$anabaena_cylindrospermum_mean 
+                                               == max(cover_list$`SFE-M`$anabaena_cylindrospermum_mean))]
+mir_m <- cover_list$`SFE-M`$field_date[which(cover_list$`SFE-M`$microcoleus_mean 
+                                              == max(cover_list$`SFE-M`$microcoleus_mean))]
 mir_ac_atx <- atx_list$`SFE-M`$field_date[which(atx_list$`SFE-M`$mean_TAC_atx == 
                                                    max(atx_list$`SFE-M`$mean_TAC_atx))]
 mir_m_atx <- atx_list$`SFE-M`$field_date[which(atx_list$`SFE-M`$mean_TM_atx == 
                                                   max(atx_list$`SFE-M`$mean_TM_atx))]
 
-# upper/miranda GPP
+# upper/miranda GPP 2023
 mir_gpp <- ggplot(data = gpp %>% filter(site_year == "sfkeel_mir_2023"), aes(x = date)) +
   geom_area(data = disc %>% filter(site_year == "sfkeel_mir_2023"), 
             aes(y = discharge_m3_s * 2), fill = "#d9ecff") +
@@ -85,10 +125,10 @@ mir_gpp <- ggplot(data = gpp %>% filter(site_year == "sfkeel_mir_2023"), aes(x =
   scale_x_date(limits = as.Date(c("2023-06-20", "2023-09-27"))) +
   scale_y_continuous(sec.axis = sec_axis(~ . / 2)) +
   coord_cartesian(ylim = c(0, 10)) +
-  labs(y = NULL, x = NULL, title = "South Fork Eel Upper")
+  labs(y = NULL, x = NULL, title = "South Fork Eel Lower 2023")
 mir_gpp
 
-## standish hickey/lower
+## standish hickey/lower 2023
 
 # dates for taxa-specific cover and anatoxin
 sth_ac <- cover_list$`SFE-SH`$field_date[which(cover_list$`SFE-SH`$anabaena_cylindrospermum_mean 
@@ -99,6 +139,11 @@ sth_ac_atx <- atx_list$`SFE-SH`$field_date[which(atx_list$`SFE-SH`$mean_TAC_atx 
                                                    max(atx_list$`SFE-SH`$mean_TAC_atx))]
 sth_m_atx <- atx_list$`SFE-SH`$field_date[which(atx_list$`SFE-SH`$mean_TM_atx == 
                                                   max(atx_list$`SFE-SH`$mean_TM_atx))]
+
+# add segment column to avoid ribbon being drawn across plot when we weren't taking data
+gpp$segment[167:168] <- 3
+gpp$segment[169:171] <- 4
+gpp$segment[172:nrow(gpp)] <- 5
 
 # lower/standish hickey GPP
 sth_gpp <- ggplot(data = gpp %>% filter(site_year == "sfkeel_sth_2023"), aes(x = date)) +
@@ -116,15 +161,28 @@ sth_gpp <- ggplot(data = gpp %>% filter(site_year == "sfkeel_sth_2023"), aes(x =
   scale_x_date(limits = as.Date(c("2023-06-20", "2023-09-27"))) +
   scale_y_continuous(sec.axis = sec_axis(~ . / 2.5)) +
   coord_cartesian(ylim = c(0, 13.6)) +
-  labs(y = NULL, x = NULL, title = "South Fork Eel Lower")
+  labs(y = NULL, x = NULL, title = "South Fork Eel Upper 2023")
 sth_gpp
+
+#### (3) Comparing Microcoleus cover & GPP ####
+
+# reload 
+sfkeel23 <- read.csv("./data/field_and_lab/sfkeel23_combined.csv") %>% 
+  mutate(field_date = ymd(field_date)) %>% 
+  filter(field_date > ymd("2023-08-01"))
+
+plot(sfkeel23$field_date, sfkeel23$GPP_median_tofourdaysprior)
+cor.test(sfkeel23$GPP_median_tofourdaysprior, sfkeel23$microcoleus)
 
 #### (3) Putting plots together & saving ####
 
 # putting two plots together
-all <- plot_grid(mir_gpp, sth_gpp, align = "v", ncol = 1)
+all <- plot_grid(mir_gpp22, mir_gpp, sth_gpp, align = "v", ncol = 1)
 all
 
 # saving
-ggsave("./figures/sfig_gpp_sfkeel_2023_notfinal.tiff", dpi = 600, 
-       width=12, height=10, unit="cm") 
+ggsave("./figures/sfig_gpp_sfkeel_peaks_notfinal.tiff", dpi = 600, 
+       width=12, height=15, unit="cm") 
+
+# curious date of max GPP in SFE-Lower 2023?
+view(gpp %>% filter(site_year == "sfkeel_mir_2023")) # 08/16/2023 > 
